c45479608afd447a2485a16b363175bf970768ab
SeanChen <xiaolaiseanchen@gmail.com>
2023-12-05 15:44:58 -0800

    Add function for user to click select destination

--- mapbox_version/main.js
+++ mapbox_version/main.js
@@ -10,6 +10,26 @@ const map = new mapboxgl.Map({
 });
 
 // Create a default Marker and add it to the map.
-const marker1 = new mapboxgl.Marker({color:'red'})
+const marker_neu = new mapboxgl.Marker({ color: 'red' })
     .setLngLat([-123.115540, 49.280747])
     .addTo(map);
+
+// Create click map event
+let selectedPoints = [];
+
+map.on('click', function(e) {
+  const coordinates = e.lngLat;
+  new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML("<button onclick='addLocation(" + JSON.stringify(coordinates) + ")'>Add this location</button>")
+      .addTo(map);
+});
+
+window.addLocation = function(coordinates) {
+  if (selectedPoints.length < 5) {
+    selectedPoints.push(coordinates);
+    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  } else {
+    alert("You can select up to 5 locations.");
+  }
+}
